5e778f3d256effb6135647af33296f89f69f0765
Thero Morwathsehla <137410274+MorwathsehlaThero2023@users.noreply.github.com>
2023-08-23 08:43:06 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -13,10 +13,3 @@
     showAllBtn.addEventListener('click', () => {
       alert('Showing all projects'); // You can replace this with your desired action
     });
-var string = "Hello World, My name is Thero Morwathsehla";
-var str = string.split("");
-var el = document.getElementById('str');
-(function animate() {
-str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running); 
-var running = setTimeout(animate, 90);
-})();
